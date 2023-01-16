5c47ffcf38ac3967e7bfc25ecadd548425642020
Vladyslav-Bekesh <114566822+Vladyslav-Bekesh@users.noreply.github.com>
2023-01-16 16:48:46 +0000

    Deploying to gh-pages from @ Vladyslav-Bekesh/IceCreamShop@9821e8075571fb4266a87d1154d5db003c798025 🚀

---
+++ index.85f80878.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-gallery",{slidesPerView:1,spaceBetween:30,direction:"horizontal",loop:!0,speed:1e3,autoplay:{speed:1200}}),new Swiper(".swiper-reviews",{slidesPerView:1,spaceBetween:100,direction:"horizontal",loop:!0,speed:2e3,grabCursor:!0,autoplay:{speed:1500},pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.85f80878.js.map
